18957a8505cb5775f55a021eff5ebab0fcad2564
Vishal Mansuriya <vishalmansuriya2004@gmail.com>
2026-01-31 16:39:37 +0530

    done 6

---
+++ exp-1/5_largest_element.py
@@ -0,0 +1,9 @@
+# Given an array arr[]. The task is to find the largest element and return it. 
+# Examples: 
+# Input: arr[] = [1, 8, 7, 56, 90] 
+# Output: 90 
+
+
+arr=[1, 8, 7, 56, 90] 
+arr.sort()
+print(arr[-1])
